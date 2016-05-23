71e6caac48c60b945f2f08620c2d9a4ad6c7cba7
Martin Koistinen <mkoistinen@gmail.com>
2016-05-22 23:16:57 -0400

    fix classifiers

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ REQUIREMENTS = []
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Web Environment',
-    'Framework :: Django' :: 1.8,
-    'Framework :: Django' :: 1.9,
+    'Framework :: Django :: 1.8',
+    'Framework :: Django :: 1.9',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
